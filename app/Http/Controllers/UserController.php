47dfd87b9032e02871f7617f61127a3205112d18
Md Laju Mia <developerlaju@gmail.com>
2023-11-10 11:08:19 +0600

    Module 7 Assingment Done

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //controller
+    public function hello()
+    {
+        return "<h1>Hello Laravel</h1>";
+    }
+}
